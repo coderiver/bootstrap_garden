4471f61e8ea4392c9ecafdd5b54d157186f5f7c9
Georgiy <goshazvir@gmail.com>
2013-10-21 14:22:31 +0300

    list

--- 02/js/common.js
+++ 02/js/common.js
@@ -19,7 +19,17 @@ $(document).ready(function() {
 		return false;
 	});
 
+	// select
 
+	$(".select").each(function(){
+		var select_first = $(this).find("option:first").text();
+		$(this).find(".select__value").text(select_first);
+
+		$(this).find("select").change(function(){
+			var select = $(this).find("option:selected").text();
+			$(this).parent().find(".select__value").text(select);
+		});
+	});
 
 });
 
