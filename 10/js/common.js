e0de6fbb3a9f6b5263fbe55f6e2cc3738e396c83
dpismenny <denis.pismenny@gmail.com>
2013-10-31 17:12:28 +0200

    start

---
+++ 10/js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+
+});
+
+$(function() {
+	var footerHeight = $(".footer").height();
+	$(".out").css("margin-bottom", -footerHeight);
+	$(".push").css("height", footerHeight);
+});
+
+$(window).resize(function() {
+	var footerHeight = $(".footer").height();
+	$(".out").css("margin-bottom", -footerHeight);
+	$(".push").css("height", footerHeight);
+});
