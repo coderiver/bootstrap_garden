d74e6ac9fc6c509c7503d0eea92341d01b7cff5f
akella <akella.a@gmail.com>
2013-11-12 17:34:19 +0200

    03 done

--- 03/js/common.js
+++ 03/js/common.js
@@ -5,9 +5,27 @@ $(document).ready(function() {
 		return $(this).length;
 	}
 
+	// slider pager
+
+	$('.carousel').carousel();
+
+	// select
+
+	$(".select").each(function(){
+		
+		var select_first = $(this).find("option:first").text();
+		$(this).find(".select__value").text(select_first);
+
+		$(this).find("select").change(function(){
+			var select = $(this).find("option:selected").text();
+			$(this).parent().find(".select__value").text(select);
+		});		
+	});
+
 	// scrollable
 	if ($('.building__gallery-wrap').exists()){
 		$('.building__gallery-wrap .caro').scrollable({
+			vertical: true,
 			next:'.slide__arrow-next',
 			prev:'.slide__arrow-prev'
 		});
