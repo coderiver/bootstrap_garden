ed043a5e245cb409e281f3413ac0bad229f71f22
Valera19900 <kaktuz34@gmail.com>
2014-02-07 19:10:11 +0200

    static

--- 05/js/common.js
+++ 05/js/common.js
@@ -15,21 +15,4 @@ $(document).ready(function() {
 		});
 	});
 
-
-	// scrollable
-		if ($('.building__gallery-wrap').exists()){
-			$('.building__gallery-wrap .caro').scrollable({
-				next:'.slide__arrow-next',
-				prev:'.slide__arrow-prev',
-				items: '.slides'
-			});
-		};
-
-		$(".building__gallery-wrap .slides a").click(function() {
-			$(".building__media img").attr('src', $(this).attr('href'));
-			$(this).parent().addClass('active').siblings().removeClass('active');
-			return false;
-		});
-
-
 });
